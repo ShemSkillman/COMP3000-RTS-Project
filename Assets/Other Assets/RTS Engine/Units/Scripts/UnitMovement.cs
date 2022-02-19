b6348aa55086e679114a0275e1d67c99dc8b03c7
Shem Skillman <43577362+ShemSkillman@users.noreply.github.com>
2022-02-19 14:39:37 +0000

    Idle swordsman fix?
    Changes:
     - IsMoving for unit is set to false when not in movement animation (should fix idle combat units bug).

--- Assets/Other Assets/RTS Engine/Units/Scripts/UnitMovement.cs
+++ Assets/Other Assets/RTS Engine/Units/Scripts/UnitMovement.cs
@@ -247,7 +247,10 @@ namespace RTSEngine.EntityComponent
 
             //to sync the unit's movement with its animation state, only handle movement if the unit is in its mvt animator state.
             if (!unit.IsInAnimatorMvtState())
+            {
+                Stop();
                 return;
+            }                
 
             UpdateMovementRotation(); 
 
