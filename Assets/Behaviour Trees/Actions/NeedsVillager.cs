dd473c282cb2aa82148e0e089d11171729d1a21a
Shem Skillman <shem.skillman@students.plymouth.ac.uk>
2022-03-08 10:37:23 +0000

    Decide villager training
    Changes:
     - AI spends one tick deciding whether a villager needs to be trained.

--- Assets/Behaviour Trees/Actions/NeedsVillager.cs
+++ Assets/Behaviour Trees/Actions/NeedsVillager.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using TheKiwiCoder;
 
-public class TrainVillager : ActionNode
+public class NeedsVillager : ActionNode
 {
+    bool madeDecision = false;
+    bool needsVillager = false;
+
     protected override void OnStart() {
     }
 
     protected override void OnStop() {
     }
 
-    protected override State OnUpdate() {
+    protected override State OnUpdate()
+    {
+        if (madeDecision)
+        {
+            madeDecision = false;
+
+            if (needsVillager)
+            {
+                return State.Success;
+            }
+            else
+            {
+                return State.Failure;
+            }
+        }
+
         int villagerCountGoal = context.factionMgr.Slot.MaxPopulation / 2;
         int villagerCount = context.factionMgr.Villagers.Count + context.factionMgr.Slot.CapitalBuilding.TaskLauncherComp.GetTaskQueueCount();
 
-        if (villagerCount < villagerCountGoal &&
+        needsVillager = villagerCount < villagerCountGoal &&
                 context.factionMgr.Slot.CapitalBuilding.TaskLauncherComp.GetTaskQueueCount() < 1 &&
                 context.gameMgr.ResourceMgr.GetFactionResources(context.factionMgr.FactionID).Resources[context.Info.IronMine.ID].GetCurrAmount() >= 100 &&
-                context.factionMgr.Slot.GetFreePopulation() > 0)
-        {
-            context.factionMgr.Slot.CapitalBuilding.TaskLauncherComp.Add(0);
+                context.factionMgr.Slot.GetFreePopulation() > 0;
 
-            return State.Running;
-        }
+        madeDecision = true;
 
-        return State.Failure;
+        return State.Running;
     }
 }
